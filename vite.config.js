9029f9fd4f1b5a3b4a7db3c5eb179a3def6ffcdc
无名路人 <mumoe@foxmail.com>
2024-04-04 22:36:13 +0800

    修改项目名称

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
 // vite.config.js 或 vite.config.ts
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
-export default defineConfig({
-  base: "/LongShotSlicer/",
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
+export default defineConfig(({ command, mode }) => {
+  //获取各种环境下的对应的变量
+  const env = loadEnv(mode, process.cwd());
+  const { VITE_APP_BASE } = env;
+
+  return {
+    base: VITE_APP_BASE || "/",
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  };
 });
